aa6520b8ddf68513f7abc679327ec3c081b911ae
Baracutanga <lgbr212@gmail.com>
2024-12-05 03:50:00 -0300

    Arrumando subcabeçalho

--- src/components/SubCabecalho/SubCabecalho.jsx
+++ src/components/SubCabecalho/SubCabecalho.jsx
@@ -1,23 +1,20 @@
-import "./styles.scss";
 import SearchBar from "../SearchBar/SearchBar";
 import React from "react";
+import "./styles.scss";
 
 const SubCabecalho = () => {
   const seforverdd = true;
   return (
     <div id="SubCabecalho">
       <SearchBar isRegister={seforverdd} />
-      <label>
       <select name="" id="">
         <option value="">Turma</option>
         <option value="1-A">1-A</option>
         <option value="2-A">2-A</option>
         <option value="1-B">1-B</option>
       </select>
-      </label>
       <button>Registrar</button>
     </div>
-    
   );
 };
 
